6e58c7219322e4a987a21a4d951ac4f448db88c4
Summer <xiaoxipu@umich.edu>
2023-04-22 15:16:42 -0700

    updated homepage, recipecard, ingrdientslists

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,66 +1,31 @@
 import React from 'react';
 import Navbar from './Navbar';
+import RecipeCard from './RecipeCard';
+import IngredientsList from './IngredientsList';
 import "../style.css";
 
 const HomePage = () => {
+  const mainRecipe = {image: 'https://bellyfull.net/wp-content/uploads/2021/02/Chicken-Alfredo-blog-4.jpg', 
+    cuisineType : 'Italian', 
+    label : 'Chicken Alfredo', 
+    calories: 300, 
+    instructions: 'https://bellyfull.net/chicken-alfredo-recipe/'}
   return(
     <div>
       <Navbar/>
-      <section className="landingPage"></section>
-      <section className="wrapper">
-        <h2 class="section-name">our delicious collections</h2>
-        <p>
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
-        our delicious collections
-        our delicious collectionsour delicious collectionsour delicious collections
+      <section className="landingPage">
+        <p style={{color: 'white'}}>
+          Chicken Alfredo
         </p>
+      </section>
+      <section className="wrapper">
+        {/* show one recipe card and list for the item in background */}
+        {/* <h5 className="section-name">View our featured recipe!</h5> */}
+        <RecipeCard {...mainRecipe}/>
+        <div className="ingredientsBox">
+          <p>Click below ingredients to add to your shopping list!</p>
+         <IngredientsList ingredients={['1 cup Flour', '1 cup water']}/>
+        </div>
       </section>
     </div>
     
